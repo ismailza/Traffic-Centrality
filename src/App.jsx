a354841bb5f863a142fde8c41a0aef57793f4f27
Ismail ZAHIR <ismailza407@gmail.com>
2024-04-03 12:56:24 +0000

    integrate multi-language support, #1

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Classification from './pages/Classification'
+import Centralization from './pages/Centralization'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
+      <Route path="/centralization" element={<Centralization />} />
       <Route path="/classification" element={<Classification />} />
     </Routes>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,15 +2,35 @@ import { useEffect, useState } from "react";
 import { CircleLoader } from "react-spinners";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { useTranslation } from "react-i18next";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const About = () => {
   const [loading, setLoading] = useState(true);
 
+  const { t } = useTranslation();
+
   const teamMembers = [
-    "Ismail ZAHIR",
-    "Khaoula ABASSI",
-    "Mohamed JEBBANEMA",
-    "El Mehdi Salah BEN SOUDA"
+    {
+      name: "Ismail ZAHIR",
+      github: "https://github.com/ismailza",
+      linkedin: "https://www.linkedin.com/in/ismailzahir01/",
+    },
+    {
+      name: "Khaoula ABASSI",
+      github: "https://github.com/Wahya1",
+      linkedin: "https://www.linkedin.com/in/khaoula-abassi/",
+    },
+    {
+      name: "Mohamed JEBBANEMA",
+      github: "https://github.com/medjebb",
+      linkedin: "https://www.linkedin.com/in/mohamed-jebbanema/",
+    },
+    {
+      name: "El Mehdi Salah BEN SOUDA",
+      github: "https://github.com/Mehdi-Ben-Souda",
+      linkedin: "https://www.linkedin.com/in/el-mehdi-salah-ben-souda/",
+    },
   ];
 
   useEffect(() => {
@@ -31,25 +51,33 @@ const About = () => {
         <div className="container my-5">
           <div className="p-5 mb-4 bg-light rounded-3 border">
             <div className="container-fluid py-5">
-              <h1 className="display-5 fw-bold">About the Kolmogorov Team</h1>
+              <h1 className="display-5 fw-bold">{t('aboutUsTitle')}</h1>
               <p className="col fs-4">
-                Discover the innovation behind our Map Centrality project. Our team focuses on creating insightful visualizations to explore network centrality in geographic data, revealing new patterns and relationships.
+                {t('aboutUsSubtitle')}
               </p>
             </div>
           </div>
 
           <section className="mb-5">
-            <h2 className="mb-4">Meet Our Team</h2>
+            <h2 className="mb-4">{t('teamSectionTitle')}</h2>
             <p className="mb-4">
-              We are a team of software engineering students at Faculty of Sciences and Technology of Mohammedia.
+              {t('aboutUsText')}
             </p>
             <div className="row row-cols-1 row-cols-md-4 g-4">
-              {teamMembers.map((name, index) => (
+              {teamMembers.map((member, index) => (
                 <div className="col" key={index}>
                   <div className="card h-100">
                     <img src="https://avatars.githubusercontent.com/u/59634965?v=4" className="card-img-top" alt={`${name}'s profile`} />
                     <div className="card-body">
-                      <h5 className="card-title">{name}</h5>
+                      <h5 className="card-title">{member.name}</h5>
+                      <div className="d-flex justify-content-around">
+                        <a href={member.github} target="_blank" aria-label="GitHub">
+                          <FaGithub /> {t('github')}
+                        </a>
+                        <a href={member.linkedin} target="_blank" aria-label="LinkedIn">
+                          <FaLinkedin /> {t('linkedin')}
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
